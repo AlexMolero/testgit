63927cb65693e734809e862a9f1a67b8b3eb1e9e
Lluis Hernandez <lluishi93@gmail.com>
2016-03-02 21:24:37 +0100

    Aprende a programar

--- Hola.java
+++ Hola.java
@@ -1,6 +1,7 @@
 public class Hola{
-	static String nombre = "Alex";
+	String nombre = "Alex";
 	public static void main(string[] args){
 		System.out.println("Hola" + nombre);
+		Hola hola = new Hola();
 	}
 }
